8a531caa2753a948520ebf663f01acb630007bcd
Prezi <Prezi@prezi.com>
2018-02-09 14:27:49 +0100

    Improvements in body declaration expression

---
+++ ViewEngine.Core/Grammar/FuncDeclaration/FuncDeclBodyTemplateLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewEngine.Core.Grammar.FuncDeclaration
+{
+    public class FuncDeclBodyTemplateLine : IFuncDeclBodyLine
+    {
+        public string TemplateLine { get; set; }
+
+        public FuncDeclBodyTemplateLine(string templateLine)
+        {
+            TemplateLine = templateLine;
+        }
+    }
+}
