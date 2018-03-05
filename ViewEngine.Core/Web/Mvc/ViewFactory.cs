19def486dbeceb95f46d605b095f08fd32fa0e11
Marek Hołyński <marek.holynski@live.com>
2018-03-05 23:36:19 +0100

    MVC package added

--- ViewEngine.Core/Web/Mvc/ViewFactory.cs
+++ ViewEngine.Core/Web/Mvc/ViewFactory.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Web.Mvc;
+using System.Web.Mvc;
 
 namespace ViewEngine.Core.Web.Mvc
 {
-    public class ViewFactory// : IViewEngine
+    public class ViewFactory : IViewEngine
     {
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
